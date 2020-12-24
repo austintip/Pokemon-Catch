bcde70f7bab7dcb1b0fc1ef7628f65dde6614ed1
austintip <austin.d.tipograph@gmail.com>
2020-12-23 19:57:30 -0500

    Pokemon Catch beginning

---
+++ Pokemon Catch/js/main.js
@@ -0,0 +1,16 @@
+console.log("hello")
+document.addEventListener('DOMContentLoaded', (e => {
+    console.log("hello!")
+}));
+// Have a box that displays instructions
+//Have a button in the box that says start
+//After click "start" the box disappears
+//have a trainer (box for now) that moves with arrow keys
+//have trainer throw a ball with spacebar
+// have team rocket grunt that moves left to right while game is played
+// have pokemon that spawns from off screen and moves across
+// have a feature that detects a catch
+//detect catch makes pokemon disappear into ball
+// have a feature that detects a collision with rocket
+//end game on rocket collision
+// display "game over" in new window with "Play again" btn
